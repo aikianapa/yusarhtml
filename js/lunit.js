401b0875b4fa946a5fe0df6e8cf2cae788ee6f1a
Baranouski <mabar@tut.by>
2021-10-01 09:26:24 +0300

    на мобилках статистика смещена ниже, к центру фото, увеличен размер шрифта

--- js/lunit.js
+++ js/lunit.js
@@ -1,8 +1,8 @@
-setTimeout(function(){
+setTimeout(function () {
   window.scrollTo(0, 0);
-},400)
+}, 400);
 $(function () {
-  var ht = $('.qbody .staging ').height();
+  var ht = $(".qbody .staging ").height();
   if ($(window).width() > 0) {
     $(".to-fix").scroolly(
       [
@@ -17,7 +17,7 @@ $(function () {
         {
           alias: "fixing",
           from: "con-top = top",
-          to: "con-top + "+ht+" = bottom",
+          to: "con-top + " + ht + " = bottom",
           css: {
             position: "fixed",
             top: "",
@@ -27,7 +27,7 @@ $(function () {
         },
         {
           alias: "unfixing",
-          from: "con-top + "+ht+" = bottom",
+          from: "con-top + " + ht + " = bottom",
           to: "doc-bottom",
           css: {
             position: "absolute",
@@ -95,19 +95,20 @@ $(function () {
       offset[i] = start + os * (i + 1);
       if (i !== 0) {
         if (i + 1 < images) {
-          $(".staging-1 .scene-2 img:eq(" + (i - 1) + ")").scroolly([
-            {
-              from: "con-top + " + (offset[i - 1]) + " = top",
-              to: "con-top + " + (offset[i]) + " = top",
-              cssFrom: {
-                "opacity": "0.999",
+          $(".staging-1 .scene-2 img:eq(" + (i - 1) + ")").scroolly(
+            [
+              {
+                from: "con-top + " + offset[i - 1] + " = top",
+                to: "con-top + " + offset[i] + " = top",
+                cssFrom: {
+                  opacity: "0.999",
+                },
+                cssTo: {
+                  opacity: "0.001",
+                  "z-index": "0",
+                },
               },
-              cssTo: {
-                "opacity": "0.001",
-                "z-index": "0"
-              },
-            }
-          ],
+            ],
             $(".staging-1")
           );
         }
@@ -115,28 +116,27 @@ $(function () {
         $(".staging-1 .scene-2 img:eq(" + i + ")").scroolly(
           [
             {
-              from: "con-top + " + (offset[i - 1]) + " = top",
-              to: "con-top + " + (offset[i]) + " = top",
+              from: "con-top + " + offset[i - 1] + " = top",
+              to: "con-top + " + offset[i] + " = top",
               cssFrom: {
-                "opacity": "0.001",
-                "z-index": 1 + i
+                opacity: "0.001",
+                "z-index": 1 + i,
               },
               cssTo: {
-                "opacity": "0.999",
-                "z-index": 1 + i
+                opacity: "0.999",
+                "z-index": 1 + i,
               },
             },
             {
-              from: "con-top + " + (offset[i + 1]) + 1 + " = top",
+              from: "con-top + " + offset[i + 1] + 1 + " = top",
               to: "con-top + " + (offset[i + 1] + 2) + " = top",
               cssFrom: {
-                "opacity": "0.999",
+                opacity: "0.999",
               },
               cssTo: {
-                "opacity": "0.001",
+                opacity: "0.001",
               },
-            }
-
+            },
           ],
           $(".staging-1")
         );
@@ -144,19 +144,19 @@ $(function () {
     }
     // ========= //
 
-
     // Плашки //
     var offset = [];
-    var plates = $('.statistics.mis-container').find('.statistics__item').length;
+    var plates = $(".statistics.mis-container").find(
+      ".statistics__item"
+    ).length;
     var height = $(".staging-1 .statistics-1").height();
     var start = 500;
     var os = Math.ceil((stop - start) / plates);
     offset[0] = start;
     for (i = 1; i <= plates; i++) {
-
       if ($(window).width() > 767) {
-        let start = 400 + (i * 60);
-        let stop = 1000 + (i * 60);
+        let start = 400 + i * 60;
+        let stop = 1000 + i * 60;
 
         $(".staging-1 .statistics__item:eq(" + (i - 1) + ")").scroolly(
           [
@@ -165,63 +165,60 @@ $(function () {
               to: "con-top + " + stop + " = top",
               cssFrom: {
                 transform: "translateY(" + i * 10 + "vh)",
-                opacity: "0"
+                opacity: "0",
               },
               cssTo: {
                 transform: "translateY(1vh)",
-                opacity: "0.7"
+                opacity: "0.7",
               },
-            }
+            },
           ],
           $(".staging-1")
         );
       } else {
-        offset[i] = start + (os * i);
-        offset[i+1] = start + (os * (i+1));
-        $('.statistics.mis-container').height(height);
-        $('.statistics.mis-container > div').height(height);
-        $('.statistics.mis-container .statistics__item').height(height);
+        offset[i] = start + os * i;
+        offset[i + 1] = start + os * (i + 1);
+        $(".statistics.mis-container").height(height);
+        $(".statistics.mis-container > div").height(height);
+        $(".statistics.mis-container .statistics__item").height(height);
         $(".staging-1 .statistics__item:eq(" + (i - 1) + ")").scroolly(
           [
             {
-              from: "con-top + " + (offset[i - 1]) + " = top",
+              from: "con-top + " + offset[i - 1] + " = top",
               to: "con-top + " + (offset[i] - 1) + " = top",
               cssFrom: {
-                top: height+"px",
-                opacity: "0.001"
+                top: height + "px",
+                opacity: "0.001",
               },
               cssTo: {
-                top: (-height)+"px",
-                opacity: "0.999"
+                top: -height + "px",
+                opacity: "0.999",
               },
             },
             {
-              from: "con-top + " + (offset[i]) + " = top",
+              from: "con-top + " + offset[i] + " = top",
               to: "con-top + " + (offset[i + 1] - 1) + " = top",
               cssFrom: {
-                opacity: "0.999"
+                opacity: "0.999",
               },
               cssTo: {
-                opacity: "0.001"
+                opacity: "0.001",
               },
-            }
-            ,
+            },
             {
-              from: "con-top + " + (offset[i + 1]) + " = top",
+              from: "con-top + " + offset[i + 1] + " = top",
               to: "con-top + " + (offset[i + 1] + 1) + " = top",
               cssFrom: {
-                top: (-height*2)+"px",
+                top: -height * 2 + "px",
               },
               cssTo: {
-                top: (-height*2)+"px",
+                top: -height * 2 + "px",
               },
-            }
+            },
           ],
           $(".staging-1")
         );
-
       }
-
     }
     /*
         $(".staging-1 .scene-2").scroolly(
