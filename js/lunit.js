d5ac5c68fc6b5c47fefde3765dc7a19fc594f840
Baranouski <mabar@tut.by>
2021-10-02 23:56:00 +0300

    синхронизация изменений анимации

--- js/lunit.js
+++ js/lunit.js
@@ -2,7 +2,9 @@ setTimeout(function () {
   window.scrollTo(0, 0);
 }, 400);
 $(function () {
-  var ht = $(".qbody .staging ").height();
+  var ht = 7000;
+  var vh = $(window).height();
+  $(".qbody .staging ").height(ht);
   if ($(window).width() > 0) {
     $(".to-fix").scroolly(
       [
@@ -86,15 +88,15 @@ $(function () {
 
     // Картинки //
     var start = 500;
-    var stop = 4000;
+    var stop = Math.ceil(ht / 2 - start);
     var images = $(".staging-1 .scene-2 img").length + 1;
     var offset = [];
 
-    for (i = 0; i < images; i++) {
+    for (i = 0; i <= images; i++) {
       let os = Math.ceil((stop - start) / images);
       offset[i] = start + os * (i + 1);
       if (i !== 0) {
-        if (i + 1 < images) {
+        if (i + 1 <= images) {
           $(".staging-1 .scene-2 img:eq(" + (i - 1) + ")").scroolly(
             [
               {
@@ -145,26 +147,28 @@ $(function () {
     // ========= //
 
     // Плашки //
-    var offset = [];
-    var plates = $(".statistics.mis-container").find(
-      ".statistics__item"
-    ).length;
-    var height = $(".staging-1 .statistics-1").height();
-    var start = 500;
-    var os = Math.ceil((stop - start) / plates);
-    offset[0] = start;
-    for (i = 1; i <= plates; i++) {
-      if ($(window).width() > 767) {
-        let start = 400 + i * 60;
-        let stop = 1000 + i * 60;
 
-        $(".staging-1 .statistics__item:eq(" + (i - 1) + ")").scroolly(
+    var height = $(".staging-1 .statistics-1").height();
+    var off = stop;
+    var top = ht / 2 - start;
+    var stop1;
+    var stop2;
+    var plates;
+    if ($(window).width() > 767) {
+      // первый блок плашек
+      plates = $(".staging-1 .statistics-1 .statistics__item").length;
+      for (i = 1; i <= plates; i++) {
+        let start = Math.ceil(top + i * ((ht / 2 - off) / plates));
+        let stop = (stop1 = start + height);
+        $(
+          ".staging-1 .statistics-1 .statistics__item:eq(" + (i - 1) + ")"
+        ).scroolly(
           [
             {
               from: "con-top + " + start + " = top",
               to: "con-top + " + stop + " = top",
               cssFrom: {
-                transform: "translateY(" + i * 10 + "vh)",
+                transform: "translateY(24vh)",
                 opacity: "0",
               },
               cssTo: {
@@ -175,7 +179,83 @@ $(function () {
           ],
           $(".staging-1")
         );
-      } else {
+      }
+
+      // второй блок плашек
+      plates = $(".staging-1 .statistics-2 .statistics__item").length;
+      top = stop1 + vh;
+      for (i = 1; i <= plates; i++) {
+        let start = Math.ceil(top + i * ((ht / 2 - off) / plates));
+        let stop = (stop2 = Math.ceil(start + height * 1.2));
+        $(
+          ".staging-1 .statistics-2 .statistics__item:eq(" + (i - 1) + ")"
+        ).scroolly(
+          [
+            {
+              from: "con-top + " + start + " = top",
+              to: "con-top + " + stop + " = top",
+              cssFrom: {
+                transform: "translateY(24vh)",
+                opacity: "0",
+              },
+              cssTo: {
+                transform: "translateY(1vh)",
+                opacity: "0.7",
+              },
+            },
+          ],
+          $(".staging-1")
+        );
+      }
+
+      // весь блок плашек наверх
+      let start = Math.ceil(stop1 + vh / 5);
+      let stop = start + vh;
+      $(".staging-1 .statistics-1").scroolly(
+        [
+          {
+            from: "con-top + " + start + " = top",
+            to: "con-top + " + stop + " = top",
+            cssFrom: {
+              top: "-0.000vh",
+              opacity: "0.999",
+            },
+            cssTo: {
+              top: "-100vh",
+              opacity: "0",
+            },
+          },
+        ],
+        $(".staging-1")
+      );
+
+      start = Math.ceil(stop2 + vh / 5);
+      stop = start + vh;
+      $(".staging-1 .statistics-2").scroolly(
+        [
+          {
+            from: "con-top + " + start + " = top",
+            to: "con-top + " + stop + " = top",
+            cssFrom: {
+              top: "-0.000vh",
+              opacity: "0.999",
+            },
+            cssTo: {
+              top: "-100vh",
+              opacity: "0",
+            },
+          },
+        ],
+        $(".staging-1")
+      );
+    } else {
+      let offset = [];
+      let plates = $(".statistics.mis-container").find(
+        ".statistics__item"
+      ).length;
+      let os = Math.ceil((stop - top) / plates);
+      offset[0] = top;
+      for (i = 1; i <= plates; i++) {
         offset[i] = start + os * i;
         offset[i + 1] = start + os * (i + 1);
         $(".statistics.mis-container").height(height);
@@ -220,38 +300,5 @@ $(function () {
         );
       }
     }
-    /*
-        $(".staging-1 .scene-2").scroolly(
-          [
-            {
-              from: "con-top + 10000 = top",
-              to: "con-top + 11000 = top",
-              cssFrom: {
-                transform: "translateY(-35vh)",
-              },
-              cssTo: {
-                transform: "translateY(-100vh)",
-              },
-            },
-          ],
-          $(".staging-1")
-        );
-    */
-
-    $(".staging-1 .mis-header-top").scroolly(
-      [
-        {
-          from: "con-top + 10000 = top",
-          to: "con-top + 11000 = top",
-          cssFrom: {
-            transform: "translateY(-0.01vh)",
-          },
-          cssTo: {
-            transform: "translateY(-20vh)",
-          },
-        },
-      ],
-      $(".staging-1")
-    );
   }
 });
