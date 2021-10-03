4ab885b8ee7c5f5b35eb6623d9297b3025cccffb
Baranouski <mabar@tut.by>
2021-10-04 00:49:35 +0300

    сведены анимация и навигация по странице с мобильного

--- js/lunit.js
+++ js/lunit.js
@@ -1,10 +1,11 @@
-setTimeout(function () {
+var width = $(window).width();
+
+var lunitInit = function () {
   window.scrollTo(0, 0);
-}, 400);
-$(function () {
-  var ht = 7000;
+  var ht = 4500;
   var vh = $(window).height();
-  $(".qbody .staging ").height(ht);
+
+  $('.qbody .staging ').height(ht);
   if ($(window).width() > 0) {
     $(".to-fix").scroolly(
       [
@@ -61,7 +62,7 @@ $(function () {
           },
           cssTo: {
             top: "216px",
-            transform: "translateY(-1500px)",
+            transform: "translateY("+vh+"px)",
           },
         },
       ],
@@ -86,31 +87,37 @@ $(function () {
       $(".staging-1")
     );
 
+
     // Картинки //
-    var start = 500;
-    var stop = Math.ceil(ht / 2 - start);
+    if ($(window).width() > 767) {
+      var start = 0;
+      var stop = Math.ceil(ht / 2 - start);
+    } else {
+      var start = 0;
+      var stop = Math.ceil(ht);
+    }
     var images = $(".staging-1 .scene-2 img").length + 1;
     var offset = [];
+
 
     for (i = 0; i <= images; i++) {
       let os = Math.ceil((stop - start) / images);
       offset[i] = start + os * (i + 1);
       if (i !== 0) {
         if (i + 1 <= images) {
-          $(".staging-1 .scene-2 img:eq(" + (i - 1) + ")").scroolly(
-            [
-              {
-                from: "con-top + " + offset[i - 1] + " = top",
-                to: "con-top + " + offset[i] + " = top",
-                cssFrom: {
-                  opacity: "0.999",
-                },
-                cssTo: {
-                  opacity: "0.001",
-                  "z-index": "0",
-                },
+          $(".staging-1 .scene-2 img:eq(" + (i - 1) + ")").scroolly([
+            {
+              from: "con-top + " + (offset[i - 1]) + " = top",
+              to: "con-top + " + (offset[i]) + " = top",
+              cssFrom: {
+                "opacity": "0.999",
               },
-            ],
+              cssTo: {
+                "opacity": "0.001",
+                "z-index": "0"
+              },
+            }
+          ],
             $(".staging-1")
           );
         }
@@ -118,27 +125,28 @@ $(function () {
         $(".staging-1 .scene-2 img:eq(" + i + ")").scroolly(
           [
             {
-              from: "con-top + " + offset[i - 1] + " = top",
-              to: "con-top + " + offset[i] + " = top",
+              from: "con-top + " + (offset[i - 1]) + " = top",
+              to: "con-top + " + (offset[i]) + " = top",
               cssFrom: {
-                opacity: "0.001",
-                "z-index": 1 + i,
+                "opacity": "0.001",
+                "z-index": 1 + i
               },
               cssTo: {
-                opacity: "0.999",
-                "z-index": 1 + i,
+                "opacity": "0.999",
+                "z-index": 1 + i
               },
             },
             {
-              from: "con-top + " + offset[i + 1] + 1 + " = top",
+              from: "con-top + " + (offset[i + 1]) + 1 + " = top",
               to: "con-top + " + (offset[i + 1] + 2) + " = top",
               cssFrom: {
-                opacity: "0.999",
+                "opacity": "0.999",
               },
               cssTo: {
-                opacity: "0.001",
+                "opacity": "0.001",
               },
-            },
+            }
+
           ],
           $(".staging-1")
         );
@@ -146,8 +154,8 @@ $(function () {
     }
     // ========= //
 
-    // Плашки //
 
+    // Плашки //  
     var height = $(".staging-1 .statistics-1").height();
     var off = stop;
     var top = ht / 2 - start;
@@ -156,53 +164,49 @@ $(function () {
     var plates;
     if ($(window).width() > 767) {
       // первый блок плашек
-      plates = $(".staging-1 .statistics-1 .statistics__item").length;
+      plates = $('.staging-1 .statistics-1 .statistics__item').length;
       for (i = 1; i <= plates; i++) {
-        let start = Math.ceil(top + i * ((ht / 2 - off) / plates));
-        let stop = (stop1 = start + height);
-        $(
-          ".staging-1 .statistics-1 .statistics__item:eq(" + (i - 1) + ")"
-        ).scroolly(
+        let start = Math.ceil(top + (i * ((ht / 2 - off) / plates)));
+        let stop = stop1 = start + height;
+        $(".staging-1 .statistics-1 .statistics__item:eq(" + (i - 1) + ")").scroolly(
           [
             {
               from: "con-top + " + start + " = top",
               to: "con-top + " + stop + " = top",
               cssFrom: {
                 transform: "translateY(24vh)",
-                opacity: "0",
+                opacity: "0"
               },
               cssTo: {
                 transform: "translateY(1vh)",
-                opacity: "0.7",
+                opacity: "0.7"
               },
-            },
+            }
           ],
           $(".staging-1")
         );
       }
 
       // второй блок плашек
-      plates = $(".staging-1 .statistics-2 .statistics__item").length;
+      plates = $('.staging-1 .statistics-2 .statistics__item').length;
       top = stop1 + vh;
       for (i = 1; i <= plates; i++) {
-        let start = Math.ceil(top + i * ((ht / 2 - off) / plates));
-        let stop = (stop2 = Math.ceil(start + height * 1.2));
-        $(
-          ".staging-1 .statistics-2 .statistics__item:eq(" + (i - 1) + ")"
-        ).scroolly(
+        let start = Math.ceil(top + (i * ((ht / 2 - off) / plates)));
+        let stop = stop2 = Math.ceil(start + height * 1.2);
+        $(".staging-1 .statistics-2 .statistics__item:eq(" + (i - 1) + ")").scroolly(
           [
             {
               from: "con-top + " + start + " = top",
               to: "con-top + " + stop + " = top",
               cssFrom: {
                 transform: "translateY(24vh)",
-                opacity: "0",
+                opacity: "0"
               },
               cssTo: {
                 transform: "translateY(1vh)",
-                opacity: "0.7",
+                opacity: "0.7"
               },
-            },
+            }
           ],
           $(".staging-1")
         );
@@ -218,13 +222,13 @@ $(function () {
             to: "con-top + " + stop + " = top",
             cssFrom: {
               top: "-0.000vh",
-              opacity: "0.999",
+              opacity: "0.999"
             },
             cssTo: {
               top: "-100vh",
-              opacity: "0",
+              opacity: "0"
             },
-          },
+          }
         ],
         $(".staging-1")
       );
@@ -238,67 +242,69 @@ $(function () {
             to: "con-top + " + stop + " = top",
             cssFrom: {
               top: "-0.000vh",
-              opacity: "0.999",
+              opacity: "0.999"
             },
             cssTo: {
               top: "-100vh",
-              opacity: "0",
+              opacity: "0"
             },
-          },
+          }
         ],
         $(".staging-1")
       );
+
     } else {
+      var top = Math.ceil(ht - vh);
+      top = vh;
       let offset = [];
-      let plates = $(".statistics.mis-container").find(
-        ".statistics__item"
-      ).length;
-      let os = Math.ceil((stop - top) / plates);
+      let plates = $('.statistics.mis-container').find('.statistics__item').length;
+      let os = Math.ceil((ht - top - vh) / plates);
       offset[0] = top;
       for (i = 1; i <= plates; i++) {
-        offset[i] = start + os * i;
-        offset[i + 1] = start + os * (i + 1);
-        $(".statistics.mis-container").height(height);
-        $(".statistics.mis-container > div").height(height);
-        $(".statistics.mis-container .statistics__item").height(height);
+        offset[i] = top + (os * i);
+        offset[i + 1] = offset[i] + os;
+        $('.statistics.mis-container').height(height);
+        $('.statistics.mis-container > div').height(height);
+        $('.statistics.mis-container .statistics__item').height(height);
         $(".staging-1 .statistics__item:eq(" + (i - 1) + ")").scroolly(
           [
             {
-              from: "con-top + " + offset[i - 1] + " = top",
-              to: "con-top + " + (offset[i] - 1) + " = top",
+              from: "con-top + " + (offset[i - 1]) + " = top",
+              to: "con-top + " + (offset[i]) + " = top",
               cssFrom: {
-                top: height + "px",
-                opacity: "0.001",
+                "top": "25.000vh",
+                "opacity": "0.001"
               },
               cssTo: {
-                top: -height + "px",
-                opacity: "0.999",
+                "top": "-0.001vh",
+                opacity: "0.999"
               },
             },
             {
-              from: "con-top + " + offset[i] + " = top",
-              to: "con-top + " + (offset[i + 1] - 1) + " = top",
+              from: "con-top + " + (offset[i]) + " = top",
+              to: "con-top + " + (offset[i + 1]) + " = top",
               cssFrom: {
-                opacity: "0.999",
+                "top": "-0.001vh",
+                "opacity": "0.999"
               },
               cssTo: {
-                opacity: "0.001",
+                "top": "-20.001vh",
+                "opacity": "0.001"
               },
-            },
-            {
-              from: "con-top + " + offset[i + 1] + " = top",
-              to: "con-top + " + (offset[i + 1] + 1) + " = top",
-              cssFrom: {
-                top: -height * 2 + "px",
-              },
-              cssTo: {
-                top: -height * 2 + "px",
-              },
-            },
+            }
           ],
           $(".staging-1")
         );
       }
     }
+
   }
-});
+}
+
+$(window).on('resize',function(){
+    if (Math.abs(width - $(window).width()) > width / 10) lunitInit();
+}) 
+
+setTimeout(function () {
+  lunitInit();
+}, 400)
--- js/main-mobile.js
+++ js/main-mobile.js
@@ -1,68 +1,75 @@
-const burger = document.querySelector(".humburger-menu");
-const menuElem = document.querySelector(".yusar-menu");
-const menuItem = document.querySelectorAll(".yusar-menu-list__item");
+$(document).ready(function () {
+  const dropDownList = document.querySelector(".drop-down-list");
+  const mobileMenuListLinks = document.querySelectorAll(
+    ".mobile-menu-list__link"
+  );
 
-const toggleMenu = () => {
-  menuElem.classList.toggle("yusar-menu-active");
-  burger.classList.toggle("humburger-menu-active");
-};
-burger.addEventListener("click", () => toggleMenu());
+  dropDownList.addEventListener("click", () =>
+    $(".mobile-menu-list").toggleClass("mobile-menu-list-active")
+  );
 
-for (let item of menuItem) {
-  item.addEventListener("click", () => toggleMenu());
-}
+  mobileMenuListLinks.forEach((link, index) => {
+    link.addEventListener("click", () => {
+      $(".mobile-menu-list").toggleClass("mobile-menu-list-active");
+    });
+  });
 
-// $(document).ready(function () {
-//   $(document).scroll(function () {
-//     const top = window.pageYOffset;
-//     const item1 = $("#purpose").offset().top - 120;
-//     const item2 = $("#descriprion").offset().top - 120;
-//     const item3 = $("#benefits").offset().top - 120;
-//     const item4 = $("#news").offset().top - 120;
-//     const item5 = $("#reviews").offset().top - 120;
+  $(document).scroll(function () {
+    const top = window.pageYOffset;
+    const item1 = $("#purpose").offset().top - 120;
+    const item2 = $("#descriprion").offset().top - 120;
+    const item3 = $("#benefits").offset().top - 120;
+    const item4 = $("#news").offset().top - 120;
+    const item5 = $("#reviews").offset().top - 120;
 
-//     function menuItemHover() {
-//       $(".mis-menu__item").mouseover(function (e) {
-//         if (!$(this).hasClass("mis-menu__item-active")) {
-//           $(this).css("background", "#333");
-//           $(this).css("color", "#fff");
-//         }
-//       });
-
-//       $(".mis-menu__item").mouseleave(function (e) {
-//         if (!$(this).hasClass("mis-menu__item-active")) {
-//           $(this).css("background", "none");
-//           $(this).css("color", "#444");
-//         }
-//       });
-//     }
-
-//     menuItemHover();
-
-//     $(".mis-menu__item").css("background", "none");
-//     $(".mis-menu__item").css("color", "#444");
-//     $(".mis-menu__item").removeClass("mis-menu__item-active");
-
-//     if (top > item5) {
-//       $("#item-5").css("background", "#7A7F83");
-//       $("#item-5").css("color", "#fff");
-//       $("#item-5").toggleClass("mis-menu__item-active");
-//     } else if (top > item4) {
-//       $("#item-4").css("background", "#7A7F83");
-//       $("#item-4").css("color", "#fff");
-//       $("#item-4").toggleClass("mis-menu__item-active");
-//     } else if (top > item3) {
-//       $("#item-3").css("background", "#7A7F83");
-//       $("#item-3").css("color", "#fff");
-//       $("#item-3").toggleClass("mis-menu__item-active");
-//     } else if (top > item2) {
-//       $("#item-2").css("background", "#7A7F83");
-//       $("#item-2").css("color", "#fff");
-//       $("#item-2").toggleClass("mis-menu__item-active");
-//     } else if (top > item1) {
-//       $("#item-1").css("background", "#7A7F83");
-//       $("#item-1").css("color", "#fff");
-//       $("#item-1").toggleClass("mis-menu__item-active");
-//     }
-//   });
-// });
+    if (top > item5) {
+      $(".drop-down-list").text(
+        "Отзывы " +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          "V"
+      );
+    } else if (top > item4) {
+      $(".drop-down-list").text(
+        "Новости " +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          "V"
+      );
+    } else if (top > item3) {
+      $(".drop-down-list").text(
+        "Преимущества " +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          "V"
+      );
+    } else if (top > item2) {
+      $(".drop-down-list").text(
+        "Описание " +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          "V"
+      );
+    } else if (top > item1) {
+      $(".drop-down-list").text(
+        "Цель " +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          String.fromCharCode(160) +
+          "V"
+      );
+    }
+  });
+});
